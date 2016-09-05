29bf47bfeb0afdd207d4dbdea7c8a0543d3aefab
Arnaud Tournier <ltearno@gmail.com>
2016-09-05 13:57:02 +0200

    hello world

--- src/main/java/fr/lteconsulting/client/Application.java
+++ src/main/java/fr/lteconsulting/client/Application.java
@@ -1,11 +1,25 @@
 package fr.lteconsulting.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class Application implements EntryPoint
 {
 	@Override
 	public void onModuleLoad()
 	{
+
+		Label label = new Label( "Saisissez du texte et appuyez sur OK" );
+		TextBox textBox = new TextBox();
+		Button button = new Button( "Ok" );
+
+		button.addClickHandler( event -> label.setText( textBox.getText() ) );
+
+		RootPanel.get().add( label );
+		RootPanel.get().add( textBox );
+		RootPanel.get().add( button );
 	}
 }
