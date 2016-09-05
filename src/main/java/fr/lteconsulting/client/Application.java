72b4ee9fc27a4d4fcac38b1cc657e55a2d2e72b7
Arnaud Tournier <ltearno@gmail.com>
2016-09-05 13:56:41 +0200

    init

---
+++ src/main/java/fr/lteconsulting/client/Application.java
@@ -0,0 +1,11 @@
+package fr.lteconsulting.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Application implements EntryPoint
+{
+	@Override
+	public void onModuleLoad()
+	{
+	}
+}
